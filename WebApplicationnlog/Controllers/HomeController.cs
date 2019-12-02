00ed7b58ef4e039fdfad8d32c9d9b927d7ee5ccb
conanl <conanl@CONANL-PC>
2019-12-02 10:45:27 +0800

    nlog

---
+++ WebApplicationnlog/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplicationnlog.Models;
+
+namespace WebApplicationnlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog injected into HomeController");
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Hello, this is the index!");
+            _logger.LogError(exception: new Exception("test"),message:"");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
